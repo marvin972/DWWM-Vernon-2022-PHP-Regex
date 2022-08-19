3f5a9ffd7b31ea9b616fe9fad85396c47da7176d
Frédéric NOËL <contact@fredericnoel.com>
2022-08-19 09:56:11 +0200

    Définition d'une variable $masque qui comprend une chaîne de caractères délimitées par des '/'

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regex</title>
+</head>
+<body>
+    <h1>Je fais des Regex</h1>
+    <?php
 
+        $masque = '/michel/i';
+
+    ?>
+</body>
+</html>
